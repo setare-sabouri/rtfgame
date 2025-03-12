4f60b8197d1457554419d8bb2cd0b659157c0290
setareh <setarehsaboory@gmail.com>
2025-03-12 02:25:04 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Experience from "./Components/Experience";
+import { Canvas } from "react-three-fiber";
+import './styles.scss';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Canvas
+        shadows
+        camera={ {
+            fov: 45,
+            near: 0.1,
+            far: 200,
+            position: [ 2.5, 4, 6 ]
+        } }
+    >
+        <Experience />
+    </Canvas>  );
 }
 
 export default App;
---
+++ src/Components/Experience.jsx
@@ -0,0 +1,16 @@
+import { OrbitControls } from '@react-three/drei'
+import Lights from './Lights.jsx'
+import Level  from './Level.jsx'
+
+export default function Experience()
+{
+    return <>
+
+        <OrbitControls makeDefault />
+
+        <Lights />
+        <Level />
+
+
+    </>
+}
