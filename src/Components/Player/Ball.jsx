23c2e7f983bc4bc5c3507308d31081c8aa4b26cd
setareh <setarehsaboory@gmail.com>
2025-03-27 02:58:38 +0100

    movements working

--- src/Components/Player/Ball.jsx
+++ src/Components/Player/Ball.jsx
@@ -9,13 +9,38 @@ const Ball = () => {
     const [subscribeKeys,getKeys] = useKeyboardControls()
     const ballRef = useRef()
     
-    useFrame(()=>{
+    useFrame((state,delta)=>{
         const {forward,backward,left,right} = getKeys()
-        ballRef.current.applyImpulse({x:0, y:0,z:0})
+
+        const Impulse={x:0, y:0,z:0}
+        const Torque={x:0, y:0,z:0}
+
+        const speed=delta* 0.5
+
+        if(forward){
+            Impulse.z += speed
+            Torque.x += speed
+        }
+        if(backward){
+            Impulse.z -= speed
+            Torque.x -= speed
+        }
+        if(right){
+            Impulse.x +=speed
+            Torque.z += speed
+        }
+
+        if(left){
+            Impulse.x -=speed
+            Torque.z -= speed
+        }
+
+        ballRef.current.applyImpulse(Impulse)
+        ballRef.current.applyTorqueImpulse(Torque)
     })
   return (
    <>
-    <RigidBody ref={ballRef} canSleep={false} colliders="ball" position={[0,1,0]} restitution={0.2} friction={1}>
+    <RigidBody ref={ballRef} canSleep={false} colliders="ball" position={[0,1,0]} restitution={0.2} friction={1} linearDamping={0.5} angularDamping={0.5}>
         <mesh castShadow>
             <icosahedronGeometry args={[0.3,1]}/>
             <meshStandardMaterial flatShading color="mediumpurple"/>
